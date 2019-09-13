44accc1f2d61638d4ebca06b1cdfb2f489bbd585
Lake Bramlett <lake.bramlett@gmail.com>
2019-09-13 08:11:47 -0700

    refactor App to be class and add kegMasterList as app state

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 import Header from './components/Header';
 import KegList from './components/KegList';
 
 import reactLogo from './assets/React-icon.png';
 
-const App = () => (
-  <BrowserRouter>
-  <Switch>
-      <div>
-        <Header />
-        <Route exact path="/" component={KegList} />
-      </div>
-    </Switch>
-    </BrowserRouter>
-);
 
+class App extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      kegsMasterList: []
+    }
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+      <Switch>
+          <div>
+            <Header />
+            <Route exact path="/" component={KegList} />
+          </div>
+        </Switch>
+        </BrowserRouter>
+    )
+  }
+}
 export default App;
